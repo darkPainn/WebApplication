e3405c10043583cd4b93fc43a3bce15881fd06d3
darkPainn <sedat_t_2@hotmail.com>
2018-03-12 11:25:03 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Web App</title>
+        <link rel="stylesheet" type="text/css" href="../scss/style.scss">
+    </head>
+    <body>
+        <header>
+            <nav id="navbar">
+                <ul>
+                    <li> <a  href="#">Home</a> </li>
+                    <li> <a href="#">Register</a> </li>
+                    <li> <a href="#">LogIn</a> </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section id="presentation">
+                <h1>Welcome to my web application</h1>
+            </section>
+            
+        </main>
+        <footer>
+            
+        </footer>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
